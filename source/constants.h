b233dde0ee731484ddbcae3eb83b80987de491ec
Geoffrey Van Landeghem <geoffrey.vl@gmail.com>
2023-06-07 23:22:50 +0200

    fix framerate to 60fps

--- source/constants.h
+++ source/constants.h
@@ -12,6 +12,9 @@ static constexpr int NR_OF_BOXES = 28;
 static constexpr int OBJ_WIDTH = 640/10;
 static constexpr int OBJ_HEIGHT = OBJ_WIDTH;
 
+const int FPS = 60;
+const int DELAY_TIME = 1000.0f / FPS;
+
 };
 
 #endif // CONSTANTS_H
--- source/main.cpp
+++ source/main.cpp
@@ -7,12 +7,19 @@ int main(int argc, char *argv[])
 	if(!game.init(Const::GAME_WIDTH, Const::GAME_HEIGHT)) {
 		return -1;
 	}
+	
 	while(game.running())
 	{
+		uint64_t frame_start =  SDL_GetTicks64();
 		game.handleEvents();
 		game.update();
 		game.render();
-		SDL_Delay(10);
+
+		uint64_t frame_time = SDL_GetTicks64() - frame_start;
+		if(frame_time < Const::DELAY_TIME)
+		{
+			SDL_Delay((int)(Const::DELAY_TIME - frame_time));
+		}
 	}
 	Logger::debug("GAME", "closing game");
 	game.clean();
